a0402fb7d69d5926b5e9ec75eff8cecd9a166f9a
Sanmay Paine <97218362+itsRikizz@users.noreply.github.com>
2023-03-21 12:14:39 +0530

    Nodejs-Marathon- Day1

---
+++ modules-1/student.js
@@ -0,0 +1,15 @@
+const getName = () => {
+  return "Sanmay Paine";
+};
+
+const getAge = () => {
+  return 25;
+};
+
+const city = () => {
+  return "Basirhat";
+};
+
+exports.getName = getName;
+exports.getAge = getAge;
+exports.city = city;
